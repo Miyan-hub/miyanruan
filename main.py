0b7dfce4aa5623b4a28a39b3c591cb4f6e994b5c
BlackJackYan1 <mochamedandryan@gmail.com>
2024-08-06 21:51:00 +0700

    Update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import *
 import google.generativeai as genai
-import os, requests
+import os, requests, json
 
 genai.configure(api_key="AIzaSyDCX8Nzh7kUvnEEbH-mt4X3dHfErPB6-x8")
 app = Flask(__name__)
@@ -10,14 +10,26 @@ model = genai.GenerativeModel('gemini-1.5-flash-latest')
 def run_main():
     return "Hello World", 200
     
-@app.route("/admin")
-def run_admin():
-    return "Hello Admin", 200
-
 @app.route("/gemini")
 def run_gemini():
     response = model.generate_content(request.args.get(""))
     return response.text, 200
+
+@app.route("/gpt")
+def run_gpt():
+    response = requests.get("https://api.guruapi.tech/ai/gpt4", params={'username': "Miyan",'query': request.args.get('')}).json()
+    return response['msg'], 200
+
+@app.route("/bing")
+def run_bing():
+    response = requests.get("https://gpt4.guruapi.tech/bing", params={'username': "Miyan",'query': request.args.get('')}).json()
+    return response['result'], 200
+
+@app.route("/dalle")
+def run_dalle():
+    response = requests.get("https://api.gurusensei.workers.dev/dream", params={'prompt': request.args.get('')})
+    open("dalle.jpg","wb").write(response.content)
+    return send_file("dalle.jpg"), 200
 
 
 
